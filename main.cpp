0f3124d2e80e22a64bae0c0112bf125189c67370
JP <ingejp@outlook.com>
2023-04-01 10:46:38 -0600

    Se agrega la funcion de multiplicar y un commit al repositorio

--- main.cpp
+++ main.cpp
@@ -3,11 +3,14 @@ using namespace std;
 void saludar();
 void sumar(int a, int b);
 float dividir(int a, int b);
+float multiplicar(int a, int b);
 int main() {
     saludar();
     sumar(5,8);
     cout<< endl;
     cout << dividir(100,4);
+    cout<< endl;
+    cout << multiplicar(4,8);
     return 0;
 }
 
@@ -23,3 +26,6 @@ float dividir(int dividendo, int divisor){
     return dividendo/divisor;
 }
 
+float multiplicar(int multiplicando, int multiplicador){
+    return multiplicando*multiplicador;
+}
